6acaa96cf2996ac58ad61f09b35a827f62c9c094
sivakumar <sivakumarpadala5@outlook.com>
2019-05-27 12:02:12 +0530

    text

--- sevensegment.py
+++ sevensegment.py
@@ -1,2 +1,11 @@
 print("Seven Segment Converter")
 
+rows=3
+cols=3
+with open('input.txt') as f:
+   data = []
+   for i in range(0, rows):
+      data.append(list(map(str, f.readline().split()[:cols])))
+print (data)
+
+
